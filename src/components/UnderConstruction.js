55e87945cf7a87076e5212d761f5001368566d47
Ronnie Gross <rnngrss1495@gmail.co>
2019-03-29 23:53:45 -0400

    added ax to under construction page

--- src/components/UnderConstruction.js
+++ src/components/UnderConstruction.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    /* align-content: center; */
+    align-items: center;
+    /* justify-content: center; */
+    h1 {
+        color: #e01065;
+        text-shadow: 2px 2px #B8F2FF;
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0 20px 0 20px;
+        text-align: center;
+    }
+    img {
+        width: 70%;
+        align-self: center;
+        margin: 0 auto;
+    }
+    .axBox {
+        display: flex;
+        justify-content: center;
+    }
+`
 
 export default class UnderConstruction extends Component {
     render() {
         return (
             <div>
-                <h3>This page is currently under construction. Check back later!</h3>
+                <Wrapper>
+                    <h1>This page is currently under construction. Check back later!</h1>
+                    <div className="axBox">
+                        <img src="https://i.imgur.com/i12HUmL.png" alt="axotle" />
+                    </div>
+                </Wrapper>
             </div>
         )
     }
