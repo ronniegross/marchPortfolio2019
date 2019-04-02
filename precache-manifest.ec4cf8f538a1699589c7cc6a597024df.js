55c49c0b5c6cbb2be2edcf557e7ea1c200d2e6f3
Ronnie Gross <rnngrss1495@gmail.co>
2019-04-01 22:33:58 -0400

    Updates

---
+++ precache-manifest.ec4cf8f538a1699589c7cc6a597024df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39d97dda683882ae177f",
+    "url": "/static/css/main.3c00997d.chunk.css"
+  },
+  {
+    "revision": "39d97dda683882ae177f",
+    "url": "/static/js/main.12cb58b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "91e207681d86e5e2b483",
+    "url": "/static/js/2.fc00ab15.chunk.js"
+  },
+  {
+    "revision": "3497f42d9fe10477aa2060f0b997e97d",
+    "url": "/index.html"
+  }
+];
